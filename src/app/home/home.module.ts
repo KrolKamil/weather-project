2986e47d08489c28805e5bc4c2fbf9b02428c212
KrolKamil <kamilkrol1997@wp.pl>
2020-03-14 19:21:00 +0100

    ?

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
 import { MaterialModule } from '../material.module';
 import { WeatherDetailsComponent } from '../weather-details/weather-details.component';
 import { WeatherIconComponent } from '../weather-icon/weather-icon.component';
-import { WeatherDetailsService } from '../weather-details.service';
-import { WeatherDetailsPipe } from '../weather-details.pipe';
 
 @NgModule({
   imports: [
+    ReactiveFormsModule,
     MaterialModule,
     CommonModule,
     FormsModule,
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {  MatFormFieldModule } from '@angular/material/form-field';
 import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button'
 
 
 @NgModule({
   exports: [
     MatInputModule,
     MatFormFieldModule,
-    MatSliderModule
+    MatSliderModule,
+    MatButtonModule
   ]
 })
 export class MaterialModule { }
